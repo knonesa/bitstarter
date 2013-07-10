40654232f4b9b58f6043070ef830406b377fa161
knonesa <kogn.ed@gmail.com>
2013-07-10 18:17:13 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs=require('fs');
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
